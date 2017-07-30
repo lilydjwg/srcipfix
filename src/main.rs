03c5ce15a1357804b98c3161b9b204a5296349d7
lilydjwg <lilydjwg@gmail.com>
2017-07-30 19:17:37 +0800

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
 extern crate nfqueue;
 extern crate pnet;
 
+use std::io::{self, Write};
+
+use pnet::packet::Packet;
 use pnet::packet::ipv4::MutableIpv4Packet;
+use pnet::packet::udp::MutableUdpPacket;
 
 fn queue_callback(msg: &nfqueue::Message, _state: &mut ()) {
   let mut data = msg.get_payload().to_owned();
-  {
+  let udp_offset = {
+    let ip_len = data.len();
     let mut pkt = MutableIpv4Packet::new(&mut data).unwrap();
     pkt.set_source("202.118.17.142".parse().unwrap());
+    ip_len - pkt.payload().len()
+  };
+  {
+    let mut pkt = MutableUdpPacket::new(&mut data[udp_offset..]).unwrap();
+    pkt.set_checksum(0);
   }
 
   msg.set_verdict_full(nfqueue::Verdict::Accept, 0, &data);
   print!(".");
+  io::stdout().flush().unwrap();
 }
 
 fn main() {
   let mut q = nfqueue::Queue::new(());
-  println!("nfqueue example program: print packets metadata and accept packets");
 
   q.open();
-  q.create_queue(0, queue_callback);
+  q.create_queue(1, queue_callback);
   q.set_mode(nfqueue::CopyMode::CopyPacket, 0xffff);
 
   q.run_loop();
